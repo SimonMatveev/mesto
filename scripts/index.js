03d4a94c5d49308cc33d7bae58dcaefe166a60bd
Gleb Orlovtsev <the.unknown.guest@yandex.com>
2023-04-12 11:13:31 +0300

    fix: second iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,28 @@
-let editButton = document.querySelector('.content-header__edit-button');
+let editButton = document.querySelector('.profile__edit-button');
 let form = document.querySelector('.popup__container');
 let popup = document.querySelector('.popup');
 let popupNameInput = document.getElementById('name');
 let popupDesc = document.getElementById('description');
 let popupClose = document.querySelector('.popup__close');
-let popupSave = document.querySelector('.popup__btn');
 let page = document.querySelector('.page');
-let profileName = document.querySelector('.content-header__title');
-let profileDescription = document.querySelector('.content-header__text');
+let profileName = document.querySelector('.profile__title');
+let profileDescription = document.querySelector('.profile__text');
+
+function handleCloseButton() {
+  popup.classList.remove('popup_opened');
+}
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  popup.classList.remove('popup_opened');
-  page.classList.remove('page_locked');
-
   profileName.textContent = popupNameInput.value;
   profileDescription.textContent = popupDesc.value;
+  handleCloseButton();
 }
 
 function handleEditButton() {
   popup.classList.add('popup_opened');
-  page.classList.add('page_locked');
-
   popupNameInput.value = profileName.textContent;
   popupDesc.value = profileDescription.textContent;
-}
-
-function handleCloseButton(evt) {
-  evt.preventDefault();
-  popup.classList.remove('popup_opened');
-  page.classList.remove('page_locked');
 }
 
 editButton.addEventListener('click', handleEditButton);
